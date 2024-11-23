8ad40f7a6bd7a02646efefdb825d50a41a58d84a
Omkar Patil <git21cs072-t@students.git.edu>
2024-11-23 18:54:25 +0530

    Added Docker And Go

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -7,7 +7,7 @@ import "net/http"
 
 func (app *application) enableCors (h http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "http://*");
+        w.Header().Set("Access-Control-Allow-Origin", "*");
 
 		//Allowing All Origins
        
