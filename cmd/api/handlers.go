ac5a7bd297f7862da95ae40ae831d10f890e82b3
Omkar Patil <git21cs072-t@students.git.edu>
2024-11-29 20:57:24 +0530

    Used errorJSON in real Case !!

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -77,7 +77,7 @@ func (app *application) AllMovies( w http.ResponseWriter, r *http.Request){
 	
 	movies,err := app.DB.AllMovies();
 	if err!=nil{
-        fmt.Println(err);
+       app.errorJSON(w,err)
 		return 
     }
 	//===========> Old Method <==================
